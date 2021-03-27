ad000ddbfa1460f179fb99993f40d74d9b50a3e3
Seokmin <bsmmoon@gmail.com>
2021-03-27 13:24:01 +0900

    reversort-eng first version

--- reversort-eng/solution.py
+++ reversort-eng/solution.py
@@ -1,6 +1,20 @@
 def solve(n, c):
-    if c not in range (n, int((n*(1+n))/2) + 1): return "IMPOSSIBLE"
-    return [4,2,1,3]
+    if c not in range (n-1, int((n*(1+n))/2)): return "IMPOSSIBLE"
+
+    counts = []
+    total = n-1  # all 1s initially
+    for i in range(n-1):
+        count = min(n - i, c - (total - 1))
+        if count == 1: break
+        counts.append(count)
+        total += count - 1
+
+    arr = list(range(1, n+1))[::-1]
+    for count in counts[::-1]:
+        arr = arr[:count][::-1] + arr[count:]
+    arr = arr[::-1]
+
+    return arr
 
 def verify(n, arr):
     if arr == "IMPOSSIBLE": return
