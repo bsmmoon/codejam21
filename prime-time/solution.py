da2b84813941c3d71da06e7f0c53c7261a9b1eae
bsmmoon <bsmmoon@gmail.com>
2021-04-10 11:38:24 +0900

    Update solution.py

--- prime-time/solution.py
+++ prime-time/solution.py
@@ -1,55 +1,32 @@
-# Assumptions
-# It is ALWAYS better to do min number of appends
-# It is ALWAYS better to do append with min digit
-# Need minimally same length of digits
-
-# Approach 1.
-
-def digits(num):
-    return len(str(num))
-
-def solve(n, arr):
-    count = 0
-    for i, num in enumerate(arr):
-        if i == 0: continue
+class Solver:
+    def solve(self, configs):
+        self.answer = 0
         
-        prev = arr[i-1]
+        cards = []
+        for config in configs:
+            cards += [config[0]] * config[1]
         
-        pprefix = int(str(prev)[:digits(num)])
-
-        if prev < num:
-            prev = num
-            continue
+        self.perm(cards, 0, 0, 1)
         
-        if prev == num:
-            num *= 10
-            count += 1
+        return self.answer
         
-        elif pprefix == num:
-            count += digits(prev) - digits(num)
-            num = prev + 1
-            
-            if str(prev)[0] != str(num)[0]:
-                num = prev * 10
-                count += 1
+    def perm(self, cards, index, summ, product):
+        if not index < len(cards):
+            if summ == product:
+                self.answer = summ
+                return True
+            return False
         
-        elif pprefix < num:
-            diff = digits(prev) - digits(num)
-            num *= pow(10, diff)
-            count += diff
-            
-        elif pprefix > num:
-            diff = digits(prev) - digits(num) + 1
-            num *= pow(10, diff)
-            count += diff
-        
-        arr[i] = num
-        prev = num
-
-    return count
+        if self.perm(cards, index+1, summ + cards[index], product) or \
+            self.perm(cards, index+1, summ, product * cards[index]): return True
+        return False
 
 t = int(input())
 for i in range(1, t+1):
-    n = int(input())
-    arr = list(map(lambda x: int(x), input().split(" ")))
-    print('Case #{}: {}'.format(i, solve(n, arr)))
+    m = int(input())
+    cards = []
+    for _ in range(m):
+        c, n = input().split(" ")
+        cards.append((int(c), int(n)))
+    
+    print('Case #{}: {}'.format(i, solve(cards)))
